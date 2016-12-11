748cee81b7498c2c961815b2d23a0929120e2a1f
Victor Quirino Lopes <qlopes92@gmail.com>
2016-12-11 16:23:53 -0200

    First commit

---
+++ js/main.controller.js
@@ -0,0 +1,38 @@
+angular.module("main-app", [])
+  .controller("MainCtrl", function($scope){
+    $scope.labelMarket = "Supermercado";
+    $scope.labelList = "Lista";
+
+    $scope.marketView = true;
+
+    var arrayProducts = {};
+    $scope.arrayProducts = [];
+
+    $scope.addProductToList = function(){
+      arrayProducts =
+        {
+          'nome': $scope.name_product,
+          'qtd': $scope.qtd_product,
+          'price': $scope.price_product
+        };
+
+      $scope.arrayProducts.push(arrayProducts);
+      
+      $scope.name_product = '';
+      $scope.qtd_product = '';
+      $scope.price_product = '';
+    };
+
+    $scope.removeProductFromList = function(index){
+      $scope.arrayProducts.splice(index, 1);
+    }
+
+    $scope.isMarketView = function(){
+      $scope.marketView = true;
+    }
+
+    $scope.isListView = function(){
+      $scope.marketView = false;
+    }
+  })
+;
